5ce0c8fe6f8d428e6b22237842edb8dcceead7d6
EdAz11 <edaz11.ea@gmail.com>
2018-03-05 03:30:04 +0000

    Movimento 2D e CameraScript
    Correção do movimento 2D para personagens, e script para a câmara acompanhar uma/várias personagens

---
+++ Cube Test 2/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	private Transform[] playerTransforms;
+
+
+	private void Start(){
+
+		GameObject[] allPlayers = GameObject.FindGameObjectsWithTag ("Player");
+		playerTransforms = new Transform[allPlayers.Length];
+		for (int i = 0; i < allPlayers.Length; i++) {
+			playerTransforms[i] = allPlayers[i].transform;
+		}
+	}
+
+	public float yOffset = 2.0f;
+	public float minDistance = 15.0f;
+
+	private float xMin, xMax, yMin, yMax;
+
+	private void LateUpdate(){
+
+		if (playerTransforms.Length == 0) {
+			Debug.Log ("No players found");
+			return;
+		}
+
+		xMin = xMax = playerTransforms[0].position.x;
+		yMin = yMax = playerTransforms[0].position.y;
+
+		for (int i = 1; i < playerTransforms.Length; i++) {
+
+			if (playerTransforms[i].position.x < xMin) {
+				xMin = playerTransforms[i].position.x;
+			}
+
+			if (playerTransforms[i].position.x > xMax) {
+				xMax = playerTransforms[i].position.x;
+			}
+
+			if (playerTransforms[i].position.y < yMin) {
+				yMin = playerTransforms[i].position.y;
+			}
+
+			if (playerTransforms[i].position.y > yMax) {
+				yMax = playerTransforms[i].position.y;
+			}
+		}
+
+		float xMiddle = (xMin + xMax) / 2;
+		float yMiddle = ((yMin + yMax) / 2) + (float) 1.5;
+		float distance = xMax - xMin;
+
+		if (distance < minDistance) {
+			distance = minDistance;
+		}
+		transform.position = new Vector3 (xMiddle, yMiddle, -distance);
+	}
+}
---
+++ Cube Test 2/Assets/Scripts/CubeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	private CharacterController controller;
+
+	private float moveSpeed = 5.0f;
+	private float verticalVelocity;
+	private float gravity = 14.0f;
+	private float jumpForce = 10.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (controller.isGrounded) {
+			verticalVelocity = -gravity * Time.deltaTime;
+			if (Input.GetButtonDown ("Jump")) {
+				verticalVelocity = jumpForce;
+			}
+		} else {
+			verticalVelocity -= gravity * Time.deltaTime;
+		}
+
+		Vector3 moveVector = Vector3.zero;
+		moveVector.x = Input.GetAxis ("Horizontal")*moveSpeed;
+		moveVector.y = verticalVelocity;
+		moveVector.z = 0f;
+		controller.Move (moveVector * Time.deltaTime);
+		}
+}
