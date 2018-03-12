eb14ab3226ae60be98cba7f8ba3ea829f3954833
EdAz11 <edaz11.ea@gmail.com>
2018-03-12 20:18:07 +0000

    Adicionadas animações e correções de código anterior
    Animações adicionadas: standing jab
    Correção de salto e de movimento

--- Cube Test 2/Assets/Scripts/cubeControl.cs
+++ Cube Test 2/Assets/Scripts/cubeControl.cs
@@ -5,75 +5,59 @@ using UnityEngine;
 public class cubeControl : MonoBehaviour
 {
 
-    [SerializeField]
-    private Rigidbody myRigidbody;
+	private Rigidbody myRigidbody;
 
-    [SerializeField]
-    private bool isJumping = false;
-    /*public bool isFalling = false;
-    public float initialGround;// = transform.position.y;
-    public float maxHeight;// = initialGround + 1;*/
+		
 
-    private bool jump = false;
+	[SerializeField]
+	private Animator animator;
 
-    private float jumpForce = 500f;
-    private float speed = 8f;
+	[SerializeField]
+    private float jumpForce = 10f;
+
+	[SerializeField]
+    private float speed = 4f;
+
+	[SerializeField]
+	private float airSpeed = 2f;
 
     // Use this for initialization
     private void Start()
     {
+		myRigidbody = GetComponent<Rigidbody> ();
+		animator = GetComponent<Animator> ();
     }
 
     // Update is called once per frame
     private void Update()
     {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		if(animator.GetBool("airborn") && moveHorizontal != 0.0f){
+			myRigidbody.velocity = new Vector3(airSpeed * moveHorizontal, myRigidbody.velocity.y, 0);
+		}
+		if(moveHorizontal != 0.0f && !(animator.GetBool("airborn"))){
+			animator.SetBool ("walking", true);
+			myRigidbody.velocity = new Vector3(speed * moveHorizontal, myRigidbody.velocity.y, 0);
+		}else{
+			animator.SetBool ("walking", false);
+		}
 
-        jump = false;
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            jump = true;
-        }
-
-        //transform.Translate(0.0f, 0.0f, 0.0f);
-        /*  if (Input.GetKey("up")) transform.Translate(0, 0, 0.25f);
-          if (Input.GetKey("down")) transform.Translate(0, 0, -0.25f);
-          if (Input.GetKey("left")) transform.Translate(-0.25f, 0, 0);
-          if (Input.GetKey("right")) transform.Translate(0.25f, 0, 0);
-          if (Input.GetKey("space") && this.isJumping == false && this.isFalling == false) this.isJumping = true;
-
-          if(this.isJumping)
-          {
-              transform.Translate(0, 0.5f, 0);
-          }
-
-          if(transform.position.y == this.maxHeight)
-          {
-              this.isJumping = false;
-              this.isFalling = true;
-          }
-
-          if (this.isFalling)
-          {
-              transform.Translate(0, -0.5f, 0);
-          }
-
-          if (transform.position.y == this.initialGround)
-          {
-              this.isJumping = false;
-              this.isFalling = false;
-          }*/
-
+		if (Input.GetButtonDown("Jump") && !(animator.GetBool ("airborn")))
+		{
+			myRigidbody.AddForce(Vector3.up*jumpForce);
+			animator.SetBool ("airborn", true);
+		}
     }
 
     private void FixedUpdate()
     {
-        if (jump)
-        {
-            myRigidbody.AddForce(Vector3.up * jumpForce);
-            jump = false;
-        }
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        myRigidbody.velocity = new Vector3(speed * moveHorizontal, 0, 0);
+		if(Input.GetButtonDown ("Fire1")){
+			animator.SetBool ("a_press", true);
+
+		}
+
+		if(myRigidbody.position.y <= 0.07){
+			animator.SetBool ("airborn", false);
+		}
     }
 }
