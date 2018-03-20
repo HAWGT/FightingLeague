8601173770448591efb062e8906c69c10f49ae57
HAWGT <r4th4ck3r@gmail.com>
2018-03-20 01:13:43 +0000

    inicio de colliders

---
+++ Cube Test 2/Assets/Scripts/CharacterColliderController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CharacterControl
+{
+    public class CharacterColliderController : MonoBehaviour
+    {
+
+        private Rigidbody myRigidBody;
+        private CharacterStateController stateController;
+
+        // Use this for initialization
+        void Start()
+        {
+            myRigidBody = GetComponent<Rigidbody>();
+            stateController = GetComponent<CharacterStateController>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnControllerColliderHit(ControllerColliderHit hit)
+        {
+            Rigidbody body = hit.collider.attachedRigidbody;
+            if (body == null || body.isKinematic)
+                return;
+            if(stateController.charState != CharacterStateController.CharState.attacking && stateController.charState != CharacterStateController.CharState.blocking)
+            {
+                print("hit confirmed");
+            }
+        }
+    }
+}
