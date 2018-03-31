c82d7ea3a4fa1dcd67a181d4c48bf517e92fd77b
HAWGT <r4th4ck3r@gmail.com>
2018-03-31 02:19:19 +0100

    base gs

--- Cube Test 2/Assets/Scripts/GameState.cs
+++ Cube Test 2/Assets/Scripts/GameState.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    private Transform[] playerTransforms;
+    [SerializeField]
+    private bool gameEnded = false;
+
+
+    private void Start()
+    {
+
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        playerTransforms = new Transform[allPlayers.Length];
+        for (int i = 0; i < allPlayers.Length; i++)
+        {
+            playerTransforms[i] = allPlayers[i].transform;
+        }
+        //print(allPlayers[0].gameObject.GetComponent<CharacterStateController>().healthPoints);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //READ HP
+        //if(player[0].healthPoints <= 0 || player[1].healthPoints <= 0) gameEnded = true;
+    }
 }
