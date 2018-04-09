085cd3ef6bb56688657cfc3f14f412bd9160f348
HAWGT <r4th4ck3r@gmail.com>
2018-04-09 18:49:15 +0100

    criacao manager

---
+++ Cube Test 2/Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    private Text text;
+
+    // Use this for initialization
+    private void Start () {
+        text = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	private void Update () {
+        float[] bars = GetComponent<GameState>().GetPlayerStatusBar();
+	}
+}
