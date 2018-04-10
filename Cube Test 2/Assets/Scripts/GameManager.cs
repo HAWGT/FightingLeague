eb56664e8d553e0eb7eb6c8a19be400ed1e84f73
HAWGT <r4th4ck3r@gmail.com>
2018-04-10 01:48:10 +0100

    z

--- Cube Test 2/Assets/Scripts/GameManager.cs
+++ Cube Test 2/Assets/Scripts/GameManager.cs
@@ -20,25 +20,6 @@ public class GameManager : MonoBehaviour
 
     }
 
-    private void Awake()
-    {
-        //Check if instance already exists
-        if (instance == null)
-
-            //if not, set instance to this
-            instance = this;
-
-        //If instance already exists and it's not this:
-        else if (instance != this)
-
-            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
-            Destroy(gameObject);
-
-        //Sets this to not be destroyed when reloading scene
-        DontDestroyOnLoad(gameObject);
-
-    }
-
     // Update is called once per frame
     private void Update()
     {
--- Cube Test 2/Assets/Scripts/UIManager.cs
+++ Cube Test 2/Assets/Scripts/UIManager.cs
@@ -6,17 +6,20 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour {
 
     private Text text;
+    private GameObject game;
 
     // Use this for initialization
     private void Start () {
+        game = GameObject.Find("Game Manager");
         text = GetComponent<Text>();
+        text.text = "Test";
     }
 	
 	// Update is called once per frame
 	private void Update () {
         
-        float[] bars = GetComponent<GameManager>().GetPlayerStatusBar();
-        text.text = bars[0] + "\t" + bars[1] + "\n" + bars[2] + "\t" + bars[3];
+        /*float[] bars = GetComponent<GameManager>().GetPlayerStatusBar();
+        text.text = bars[0] + "\t" + bars[1] + "\n" + bars[2] + "\t" + bars[3];*/
         
     }
 }
