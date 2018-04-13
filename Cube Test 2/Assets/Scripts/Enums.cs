b5d420d160ed704bc7402f526148bed1127f1904
EdAz11 <edaz11.ea@gmail.com>
2018-04-13 19:06:40 +0100

    Criação da class Enums

---
+++ Cube Test 2/Assets/Scripts/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Resource
+{
+    class Enums
+    {
+        public enum CharState
+        {
+            standing, walkingF, walkingB, crouching, airborn, blocking, attacking, hitstun
+        }
+
+        public enum AttackState
+        {
+            none, light, medium, heavy, special1, special2
+		}
+
+        public enum Inputs
+        {
+            Neutral, Forward, Backward, Up, Down, DownBack, DownForward, Attack
+        }
+
+        public enum FacingSide
+        {
+            P1, P2
+        }
+    }
+}
