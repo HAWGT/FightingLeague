f4311dbae054072a43ccc413555cc4accf700574
EdAz11 <edaz11.ea@gmail.com>
2018-05-07 06:15:07 +0100

    QoL

--- Cube Test 2/Assets/Scripts/Animator Scripts/FallingScript.cs
+++ Cube Test 2/Assets/Scripts/Animator Scripts/FallingScript.cs
@@ -10,9 +10,7 @@ public class FallingScript : StateMachineBehaviour {
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        animator.applyRootMotion = false;
-        rigidbody = animator.GetComponentInParent<Rigidbody>();
-        sphereCollider = rigidbody.GetComponentInParent<SphereCollider>();
+       
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
