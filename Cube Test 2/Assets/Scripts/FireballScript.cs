53cdf74bb47d44deb096edfb630af1fa80e998fd
HAWGT <r4th4ck3r@gmail.com>
2018-06-12 02:11:02 +0100

    ooh sparkles

--- Cube Test 2/Assets/Scripts/FireballScript.cs
+++ Cube Test 2/Assets/Scripts/FireballScript.cs
@@ -8,6 +8,12 @@ namespace CharacterControl
     {
         private Rigidbody creator;
 
+        [SerializeField]
+        private GameObject explosionPrefab;
+
+        [SerializeField]
+        private Collider ownCollider;
+
         public void SetCreator(Rigidbody rb)
         {
             this.creator = rb;
@@ -27,11 +33,17 @@ namespace CharacterControl
             }
             else if (body != creator)
             {
+                ownCollider.enabled = false;
                 Destroy(gameObject);
                 if (body.GetComponent<CharacterStateController>().GetCharState() != Enums.CharState.blocking)
                 {
                     body.GetComponent<CharacterStateController>().TakeDamage(1500);
                 }
+                ContactPoint contact = collision.contacts[0];
+                Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+                Vector3 pos = body.position;
+                var explosion = (GameObject)Instantiate(explosionPrefab, pos, rot);
+                Destroy(explosion, 0.25f);
             }
         }
 
--- Cube Test 2/Assets/Scripts/SpinKickScript.cs
+++ Cube Test 2/Assets/Scripts/SpinKickScript.cs
@@ -10,6 +10,12 @@ namespace CharacterControl
 
         private Rigidbody creator;
 
+        [SerializeField]
+        private GameObject explosionPrefab;
+
+        [SerializeField]
+        private Collider ownCollider;
+
         public void SetCreator(Rigidbody rb)
         {
             this.creator = rb;
@@ -28,11 +34,18 @@ namespace CharacterControl
             }
             else if (body != creator)
             {
+                ownCollider.enabled = false;
                 Destroy(gameObject);
                 if (body.GetComponent<CharacterStateController>().GetCharState() != Enums.CharState.blocking)
                 {
                     body.GetComponent<CharacterStateController>().TakeDamage(1500);
                 }
+                ContactPoint contact = collision.contacts[0];
+                Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+                Vector3 pos = contact.point;
+                var explosion = (GameObject)Instantiate(explosionPrefab, pos, rot);
+                
+                Destroy(explosion, 0.25f);
             }
         }
     }
